ebe4346de5be632336a18946fcea40c837042734
kshirod174 <kshirodsahoo98@gmail.com>
2024-03-18 18:12:54 +0530

    Datat types

--- 01_Basics/01_Variables.js
+++ 01_Basics/01_Variables.js
@@ -2,3 +2,7 @@ const accountId = 107484
 let accountEmail = "kshirod.sahoo@gamil.com"
 var accountPassword = "12345"
 accoutCity = "Bhubaneswar"
+let accountstate
+
+console.log("Email is"+accountEmail)
+//console.table([accountId,accountEmail,accountPassword,accoutCity,accountstate])
---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; //write this line to treat all JS code as newer version
+
+//alert(3 + 3) //we are using node.js and not browser, this will not work for now - we will see later different implementation of alert
+
+//different data types
+//number =>2 to the power of 53
+//bigint
+//string = ""
+//null - is a null value- it is an object
+//undefined - value not defined yet
+//symbol = unique
+
+//non-prmitive data type
+//object
+
+console.log(typeof "Kshirod")
+console.log(typeof 8)
+console.log(typeof null)
+console.log(typeof undefined)
